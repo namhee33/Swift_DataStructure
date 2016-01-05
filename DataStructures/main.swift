32906914e0b28fdf30bbb7cb980b70647cfd70f5
namhee <namhee33@gmail.com>
2016-01-05 15:00:42 -0800

    Initial Commit

---
+++ DataStructures/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  DataStructures
+//
+//  Created by namhee kim on 1/5/16.
+//  Copyright © 2016 namhee kim. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
